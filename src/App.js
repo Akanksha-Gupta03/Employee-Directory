f057124a0f6e659d107df9158237b6459e48cf1e
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-02 00:29:57 -0400

    added more components navBar,employeeData and searchName and css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import Main from "./components/Main";
-
 import './App.css';
 
 function App() {
