8fc8ecfc534756f61df2d12f44b7e9c44f09c8c2
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-03 09:27:14 -0400

    modified header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,20 @@ import React from "react";
 
 function Header (){
     const headerStyle = {
-        padding: "30px 0",
         color: "#ffffff",
-        width: "100%",
+        backgroundImage: 'url("https://ak6.picdn.net/shutterstock/videos/2757836/thumb/1.jpg")',
+        backgroundSize: "cover",
+        height:"70vh",
+        opacity:"0.8",
         textAlign: "center",
-        backgroundColor: "#111E45",
         marginBottom: "10px",
-        borderBottom: "5px solid red"
     }
     return(
-        <div className="header" style={headerStyle}>
-            <h1><span><i class="fa fa-address-book-o" aria-hidden="true"></i></span>  Employee Directory</h1>
-        </div>
+        <div className="jumbotron jumbotron-fluid" style={headerStyle}>
+          <div className="container  header">
+            <h1 style={{fontSize:"80px", fontWeight:"bolder"}}><span><i class="fa fa-address-book-o" aria-hidden="true" style={{fontSize:"80px"}}></i></span>  EMPLOYEE DIRECTORY</h1>
+          </div>
+       </div>
     )
 }
 
