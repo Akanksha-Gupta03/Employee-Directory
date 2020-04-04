796ba4c1ce6c09b6a7bb8baf46627be8fdde1f71
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-03 22:25:44 -0400

    modified css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,16 @@ import React from "react";
 
 function Header (){
     const headerStyle = {
+        padding: "30px 0",
         color: "#ffffff",
-        backgroundImage: 'url("https://ak6.picdn.net/shutterstock/videos/2757836/thumb/1.jpg")',
-        backgroundSize: "cover",
-        height:"70vh",
-        opacity:"0.8",
+        backgroundImage:"linear-gradient(to right, rgba(0,0,255,1),rgba(0,0,0,255))",
+        width:"100%",
         textAlign: "center",
-        marginBottom: "10px",
+        marginBottom: "20px",
     }
     return(
-        <div className="jumbotron jumbotron-fluid" style={headerStyle}>
-          <div className="container  header">
+        <div className="header" style={headerStyle}>
             <h1 style={{fontSize:"80px", fontWeight:"bolder"}}><span><i class="fa fa-address-book-o" aria-hidden="true" style={{fontSize:"80px"}}></i></span>  EMPLOYEE DIRECTORY</h1>
-          </div>
        </div>
     )
 }
--- src/components/SearchName.js
+++ src/components/SearchName.js
@@ -1,25 +1,15 @@
 import React, { useContext } from "react";
-import "../styles/SearchName.css";
 import DeveloperContext from "../utils/DeveloperContext";
 
 const SearchName = () => {
   const context = useContext(DeveloperContext);
 
   return (
-    <div className="searchbox">
+    <div className="searchbox" style={{width: "100%"}}>
       <div className="input-group">
-          <div className="input-group-prepend">
-          <button class="btn btn-outline-secondary" type="button" id="button-addon1">Search</button>
-          </div>
-          <input className="form-control mr-sm-2" type="search" placeholder="name" aria-label="Search" onChange={e => context.handleSearchChange(e)}/>
+          <input className="form-control" type="search" placeholder="search your employee" aria-label="Search" style={{borderRadius:"50px"}} onChange={e => context.handleSearchChange(e)}/>
         </div>
     </div>
   );
 }     
-//     <div class="input-group input-group-sm mb-3">
-//     <div class="input-group-prepend">
-//       <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
-//     </div>
-//     <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-//   </div>
 export default SearchName;
