181691af7ef598fd142b9e3bfca6b14baf2738f1
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-06 15:07:47 -0400

    Updates

---
+++ precache-manifest.0fed241a781ec59f0ce013177fcdbb78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b15d0f9736df1601b7c427ec3dfbd2b",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "f2bd1782b7b7333e905d",
+    "url": "/Employee-Directory/static/css/main.a58378f9.chunk.css"
+  },
+  {
+    "revision": "c8c3cb24d309a648faff",
+    "url": "/Employee-Directory/static/js/2.b09ccdcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.b09ccdcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2bd1782b7b7333e905d",
+    "url": "/Employee-Directory/static/js/main.4b043e9f.chunk.js"
+  },
+  {
+    "revision": "961815129ed41eb07899",
+    "url": "/Employee-Directory/static/js/runtime-main.9bb5a6c6.js"
+  }
+]);
