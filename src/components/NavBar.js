15a2f8fb18223f7b6a5afc4858aaf352e5a7b179
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-06 09:33:21 -0400

    updated readme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import "../styles/NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-darkt bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <button
         className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
